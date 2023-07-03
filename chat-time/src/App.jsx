fb93b22ebf2b2c3f8293625b2054f9cdcce3824c
Amritesh Dasari <mdasari2@illinois.edu>
2023-07-03 14:53:57 -0500

    Initial Update

---
+++ chat-time/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Room from './pages/Room'
+
+function App() {
+
+  return (
+    <>
+    <Room/>
+    </>
+  )
+}
+
+export default App
---
+++ chat-time/src/appwriteConfig.js
@@ -0,0 +1,13 @@
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '649cdbb0800241eb0e88'
+export const DATABASE_ID= '649cdf38e8dcd9424856'
+export const COLLECTION_ID_MESSAGES ='649cdf41853f09826396'
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('649cdbb0800241eb0e88');
+export const databases = new Databases(client);
+export default client;
